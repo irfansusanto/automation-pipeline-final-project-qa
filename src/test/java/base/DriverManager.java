24003b8f2a65d8605f23345d27384580abda8cb5
irfansusanto <irfansusanto669@gmail.com>
2026-02-01 05:29:53 +0700

    Update automation.yml

--- src/test/java/base/DriverManager.java
+++ src/test/java/base/DriverManager.java
@@ -1,6 +1,7 @@
 package base;
 
 import config.ConfigReader;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,12 +9,23 @@ import org.openqa.selenium.chrome.ChromeOptions;
 public class DriverManager {
 
     public static WebDriver createDriver() {
+
         ChromeOptions options = new ChromeOptions();
+
+        // WAJIB untuk GitHub Actions / Linux
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        // Headless mode (CI)
         if (ConfigReader.isHeadless()) {
             options.addArguments("--headless=new");
         }
 
-        options.addArguments("--start-maximized");
+        // Auto download driver
+        WebDriverManager.chromedriver().setup();
+
         return new ChromeDriver(options);
     }
 }
